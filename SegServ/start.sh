4bacf1615fbfd562d1f52960b568721208b379a5
Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
2017-11-21 19:02:22 +0000

    Updates

--- SegServ/start.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-REALTIME=/home/localek10/workspace/Projs/BramRealTime/realtime-20171108162848/realtime.meta
-DLTK=/home/localek10/workspace/Projs/SegmentTests/dltk-20171114184156/dltk.meta
-
-$HOME/anaconda2/bin/python segserv.py $REALTIME $DLTK
