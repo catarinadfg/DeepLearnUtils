32f874fcd86d14b604c5b69f9c4167df5b739611
Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
2017-12-25 02:46:11 +0000

    Updates

--- tfutils.py
+++ tfutils.py
@@ -1,6 +1,17 @@
 
 from __future__ import division, print_function
 import tensorflow as tf
+import numpy as np
+import os
+import datetime
+import threading
+import time
+import collections
+
+try:
+    import Queue as queue
+except:
+    import queue
 
 
 def averageGradients(tower_grads):
@@ -32,3 +43,123 @@ def binaryMaskDiceLoss(logits, labels, smooth=1e-5):
     dice = tf.reduce_mean((2.0 * intersection + smooth) / (sums + smooth))
     return 1.0-dice
 
+
+class GraphImageHook(tf.train.SessionRunHook):
+    def __init__(self, fetches,graphnames,imagenames):
+        self.fetches = fetches
+        self.graphnames=graphnames
+        self.graphvalues=collections.OrderedDict([(n,[]) for n in graphnames]+[(n+' Avg',[]) for n in graphnames])
+        self.imagenames=imagenames
+        self.avgLength=50
+        self.images={}
+        
+    def before_run(self, run_context):
+        return tf.train.SessionRunArgs(self.fetches)
+    
+    def after_run(self, run_context, run_values):
+        res=run_values.results
+        
+        for n in self.graphnames:
+            v=res[n]
+            self.graphvalues[n].append(v)
+            rollingAvg=np.average(self.graphvalues[n][-self.avgLength:])
+            self.graphvalues[n+' Avg'].append(rollingAvg)
+            
+        self.images=collections.OrderedDict((n,res[n]) for n in self.imagenames)
+        self.update()
+        
+    def update(self):
+        pass
+
+
+class BinarySegmentNN(tf.estimator.Estimator):
+    def __init__(self,savedirprefix=None,runconf=None,params={}):
+        self.savedir=None
+        self.imgs=None
+        self.masks=None
+        self.loss=None
+        self.logits=None
+        self.preds=None
+        self.opt=None
+        self.trainop=None
+        self.runconf=runconf
+        self.summaries={}
+        
+        self.logfilename='train.log'
+        self.logqueue=queue.Queue()
+        self.logthread=None
+        
+        def _printlog():
+            while True:
+                time.sleep(0.1)
+                if os.path.isdir(self.savedir): # directory won't appear until training starts, defer writing log file until then
+                    with open(os.path.join(self.savedir,self.logfilename),'a') as o:
+                        while not self.logqueue.empty():
+                            o.write(self.logqueue.get()+'\n')
+
+        if savedirprefix:
+            if os.path.exists(savedirprefix):
+                self.savedir=savedirprefix
+            else:
+                self.savedir='%s-%s'%(savedirprefix,datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
+                
+            self.logthread=threading.Thread(target=_printlog)
+            self.logthread.daemon=True
+            self.logthread.start()
+
+        tf.estimator.Estimator.__init__(self, model_fn=self._modelfn, model_dir=self.savedir,params=params, config=self.runconf)
+        
+    def log(self,*items):
+        dt=datetime.datetime.now().strftime('%Y%m%d-%H:%M:%S: ')
+        self.logqueue.put(dt+' '.join(map(str,items)))
+
+    def _modelfn(self,features, labels, mode, params):
+        global_step = tf.train.get_global_step()
+        self.opt = tf.train.AdamOptimizer(learning_rate=params.get('learning_rate',1e-3),epsilon=1e-5)
+        
+        try:
+            self.imgs=features.values()[0]
+        except:
+            self.imgs=features
+            
+        self.masks=labels
+        self.createNetwork(mode,params)
+        net={'logits':self.logits,'preds':self.preds}
+
+        with tf.variable_scope(tf.get_variable_scope(),reuse=tf.AUTO_REUSE):
+            if mode == tf.estimator.ModeKeys.PREDICT:
+                outs={'out': tf.estimator.export.PredictOutput(net)}
+                return tf.estimator.EstimatorSpec( mode=mode, predictions=net, export_outputs=outs)
+            else:
+                self.loss=binaryMaskDiceLoss(self.logits,self.masks)
+
+                update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+                with tf.control_dependencies(update_ops):
+                    self.trainop=self.opt.minimize(self.loss, global_step=global_step)
+
+                tf.add_to_collection('endpoints',self.imgs)
+                tf.add_to_collection('endpoints',self.masks)
+                tf.add_to_collection('endpoints',self.logits)
+                tf.add_to_collection('endpoints',self.preds)
+                tf.add_to_collection('endpoints',self.trainop)
+                tf.add_to_collection('endpoints',self.loss)
+
+                self.summaries.clear()
+                self.summaries['imgs'] = self.imgs[0, 0, :, :, 0]
+                self.summaries['masks'] = tf.cast(self.masks, tf.float32)[0, 0, :, :]
+                self.summaries['logits'] = self.logits[0, 0, :, :,0]
+                self.summaries['preds'] = tf.cast(self.preds, tf.float32)[0, 0, :, :]
+                
+                for name, image in self.summaries.items():
+                    shape=[1,image.shape[0],image.shape[1],1]
+                    tf.summary.image(name, tf.reshape(image, shape))
+            
+                self.summaries['loss']=self.loss
+                tf.summary.scalar('loss',self.loss)
+
+                return tf.estimator.EstimatorSpec(mode=mode, predictions=net, loss=self.loss, train_op=self.trainop, eval_metric_ops=None)
+        
+    def createNetwork(self,mode,params):
+        pass
+    
+    
