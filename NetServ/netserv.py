0ffdbe1c83a648cb21f485292d254797da3e5051
Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
2019-03-04 14:57:32 +0000

    Updates

--- NetServ/netserv.py
+++ NetServ/netserv.py
@@ -31,31 +31,98 @@ or using forms as such:
 
 '''
 from __future__ import division, print_function
-import io, argparse, ast, logging, importlib.util
+import io, argparse, ast, logging, requests, urllib, importlib.util
 
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, jsonify
 
 import numpy as np
 
 from imageio import imwrite, imread
 
+
+class InferenceClient(object):
+    def __init__(self,host,port):
+        self.host=str(host)
+        self.port=int(port)
+        self.url='http://%s:%i'%(self.host,self.port)
+        
+        req=requests.get(self.url+'/list')
+        req.raise_for_status()
+            
+        self.names=req.json()
+        
+    def getInfo(self,name):
+        req=requests.get('%s/info/%s'%(self.url,name))
+        req.raise_for_status()
+        
+        return req.json()
+    
+    def getInferImage(self,name,img,**kwargs):
+        stream=io.BytesIO()
+        imwrite(stream,img,format='png') # encode image as png
+        stream.seek(0)
+
+        fullurl='%s/inferimg/%s?%s'%(self.url,name,urllib.parse.urlencode(kwargs))
+        
+        req=requests.post(fullurl,headers={'Content-Type':'image/png'},data=stream.read()) # post image data
+        req.raise_for_status()
+         
+        return imread(io.BytesIO(req.content)) # return image read from response byte stream
+        
+
+class InferenceContainer(object):
+    def __init__(self,description,inputMap,outputMap,argMap):
+        self.description=description
+        self.inputMap=inputMap
+        self.outputMap=outputMap
+        self.argMap=argMap
+        
+    def infer(self,*inputMatrices,**kwargs):
+        pass
+    
+    
+class EchoContainer(InferenceContainer):
+    def __init__(self):
+        super().__init__('Echo test container',{'in':(999,999,3)},{'out':(999,999,3)},{})
+        
+    def infer(self,*inputMatrices,**kwargs):
+        return np.squeeze(inputMatrices[0])
+    
+
 app = Flask(__name__)
-
-infermap={
-        'echo':lambda i,**kw:np.squeeze(i) # add a "network" which simply returns the first channel of any input image
-}
+containers={ 'echo': EchoContainer() }
 
 
-@app.route('/infer/<name>', methods=['POST'])
-def infer(name):
-    obj=infermap[name]
+@app.route('/list')
+def listContainers():
+    return jsonify(list(containers.keys()))
+    
+
+@app.route('/info/<name>')
+def info(name):
+    obj=containers[name]
+    
+    infoMap={
+        'name':name,
+        'description':obj.description,
+        'inputs':obj.inputMap,
+        'outputs':obj.outputMap,
+        'arguments':obj.argMap
+    }
+    
+    return jsonify(infoMap)
+
+
+@app.route('/inferimg/<name>', methods=['POST'])
+def inferimg(name):
+    obj=containers[name]
     args={k:ast.literal_eval(v) for k,v in request.args.items()} # keep only one value per argument name
         
     data=request.data or request.files['data'].read()
     imgmat=imread(io.BytesIO(data)) # read posted image file to matrix
     logging.info('infer(): %r %r %r %r %r %r'%(name,imgmat.shape,imgmat.dtype,imgmat.min(),imgmat.max(),args))
 
-    result=obj(imgmat,**args) # apply inference
+    result=obj.infer(imgmat,**args) # apply inference
     
     stream=io.BytesIO()
     imwrite(stream,result,format='png') # save result to png file stream
@@ -77,10 +144,10 @@ if __name__=='__main__':
         mod=importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
         
-        # update infermap with the returned inference objects
-        infermap.update(mod.getInferObjects())
+        # update containers with the returned inference objects
+        containers.update(mod.getContainerMap())
         
-    logging.info('Running server with networks %r'%(list(infermap.keys()),))
+    logging.info('Running server with networks %r'%(list(containers.keys()),))
     app.run(host=args.host,port=args.port)
     
    
