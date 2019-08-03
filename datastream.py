ae8eb0bafc380f3bc06ef005853f1fc15718891b
ericspod <eric.kerfoot@gmail.com>
2019-08-04 00:47:08 +0100

    Update datastream.py

--- datastream.py
+++ datastream.py
@@ -39,16 +39,6 @@ class DataStream(object):
         if hasattr(self.src,'stop'):
             self.src.stop()
         
-
-class RandomGenerator(DataStream):
-    """Randomly generate float32 arrays of the given shape."""
-    def __init__(self,*shape):
-        super().__init__(self.generateRandArray(shape))
-
-    def generateRandArray(self,shape):
-        while self.isRunning:
-            yield np.random.rand(*shape)
-        
     
 class FuncStream(DataStream):
     """For use with `streamgen`, the given callable is used as the generator in place of generate()."""
@@ -73,6 +63,73 @@ def streamgen(func):
         return FuncStream(src,func,args,kwargs)
     
     return _wrapper
+        
+
+class ArraySource(DataStream):
+    SHUFFLE='shuffle'
+    CHOICE='choice'
+    NONE='none'
+    
+    def __init__(self,*arrays,orderType=NONE,doOnce=False,choiceProbs=None):
+        self.arrays=tuple(map(np.atleast_1d,arrays))
+        self.arrayLen=self.arrays[0].shape[0]
+        
+        assert all(arr.shape[0]==self.arrayLen for arr in self.arrays), \
+            'All input arrays must have the same length for dimension 0.'
+            
+        assert orderType in (self.SHUFFLE,self.CHOICE,self.NONE)
+        
+        self.indices=np.arange(self.arrayLen)
+        self.orderType=orderType
+        self.doOnce=doOnce
+        
+        if choiceProbs is not None:
+            self.choiceProbs=np.atleast_1d(choiceProbs)
+            self.choiceProbs=self.choiceProbs/self.choiceProbs.sum()
+        else:
+            self.choiceProbs=None
+        
+        super().__init__(self.yieldArrays())
+        
+    def yieldArrays(self):
+        while self.isRunning:
+            if self.orderType==self.SHUFFLE:
+                np.random.shuffle(self.indices)
+            elif self.orderType==self.CHOICE:
+                self.indices=np.random.choice(range(self.arrayLen),self.arrayLen,p=self.choiceProbs)
+                
+            for i in self.indices:
+                yield tuple(arr[i] for arr in self.arrays)
+                
+            if self.doOnce:
+                break
+                
+                
+class NPZFileSource(ArraySource):
+    def __init__(self,fileName,arrayNames,orderType=ArraySource.NONE,doOnce=False):
+        self.fileName=fileName
+        
+        dat=np.load(fileName)
+        
+        keys=set(dat.keys())
+        missing=set(arrayNames)-keys
+        
+        if missing:
+            raise ValueError('Array name(s) %r not in loaded npz file'%(missing,))
+                
+        arrays=[dat[name] for name in arrayNames]
+        
+        super().__init__(*arrays,orderType=orderType,doOnce=doOnce)
+        
+
+class RandomGenerator(DataStream):
+    """Randomly generate float32 arrays of the given shape."""
+    def __init__(self,*shape):
+        super().__init__(self.generateRandArray(shape))
+
+    def generateRandArray(self,shape):
+        while self.isRunning:
+            yield np.random.rand(*shape)
     
     
 class TestImageGenerator(DataStream):
@@ -114,6 +171,9 @@ class ThreadAugmentStream(AugmentStream):
         self.numThreads=numThreads
         self.batchArrays=None
         
+    def getLocalGen(self):
+        return super().__iter__()
+        
     def _applyAugments(self,arrays):
         for a in self.generate(arrays):
             return a
@@ -126,6 +186,7 @@ class ThreadAugmentStream(AugmentStream):
         
     def __iter__(self):
         srcVals=[]
+        
         with ThreadPool(self.numThreads) as tp:
             for srcVal in self.src:
                 srcVals.append(srcVal)
